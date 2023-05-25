ff10eeeb21ce117c89d18f4ca0487fdef98e219b
szlzs1005 <szabolaszlozsolt1005@gmail.com>
2023-05-25 19:24:27 +0200

    Task 3

--- main.tf
+++ main.tf
@@ -10,3 +10,18 @@ module "read" {
   source        = "./modules/read"
   input_variable = module.files.file_content_md5
 }
+
+//Task 3
+module "write" {
+  source = "./modules/write"
+
+  answer1 = var.answers[0]
+  answer2 = var.answers[1]
+  answer3 = var.answers[2]
+  answer4 = var.answers[3]
+  answer5 = var.answers[4]
+}
+
+output "all_answers" {
+  value = module.write.all_answers
+}
